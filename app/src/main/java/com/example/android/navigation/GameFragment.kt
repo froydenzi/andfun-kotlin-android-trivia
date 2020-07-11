ac3b5b517f547a6e2970c37c6a784423859d67d4
Emir Graca <froydenzi@gmail.com>
2020-07-11 12:25:09 +0200

    Navigating through fragments

--- app/src/main/java/com/example/android/navigation/GameFragment.kt
+++ app/src/main/java/com/example/android/navigation/GameFragment.kt
@@ -23,7 +23,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.findNavController
+import androidx.navigation.Navigation
 import com.example.android.navigation.databinding.FragmentGameBinding
 
 class GameFragment : Fragment() {
@@ -97,16 +97,10 @@ class GameFragment : Fragment() {
                         setQuestion()
                         binding.invalidateAll()
                     } else {
-                        // We've won!  Navigate to the gameWonFragment.
-                        // TODO (05) Find the navController from the view and navigate to the gameWonFragment
-                        // call view.findNavController
-                        // navigate to R.id.action_gameFragment_to_gameWonFragment
+                        Navigation.findNavController(view).navigate(R.id.action_gameFragment_to_gameWonFragment)
                     }
                 } else {
-                    // Game over! A wrong answer sends us to the gameOverFragment.
-                    // TODO (06) Find the navController from the view and navigate to the gameOverFragment
-                    // call view.findNavController
-                    // navigate to R.id.action_gameFragment_to_gameOverFragment
+                    Navigation.findNavController(view).navigate(R.id.action_gameFragment_to_gameOverFragment)
                 }
             }
         }
