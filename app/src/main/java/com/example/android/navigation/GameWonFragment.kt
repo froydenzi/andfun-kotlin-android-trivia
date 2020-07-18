7fe5ec2412eaf5f45e349aedd9dd65fd51f45da3
Emir Graca <froydenzi@gmail.com>
2020-07-18 21:31:44 +0200

    Updating code

--- app/src/main/java/com/example/android/navigation/GameWonFragment.kt
+++ app/src/main/java/com/example/android/navigation/GameWonFragment.kt
@@ -44,7 +44,7 @@ class GameWonFragment : Fragment() {
                 inflater, R.layout.fragment_game_won, container, false)
         binding.nextMatchButton.setOnClickListener { view: View? ->
             if (view != null) {
-                view.findNavController().navigate(R.id.action_gameWonFragment_to_gameFragment)
+                view.findNavController().navigate(GameWonFragmentDirections.actionGameWonFragmentToGameFragment())
             }
         }
         val args = arguments?.let { GameWonFragmentArgs.fromBundle(it) }
